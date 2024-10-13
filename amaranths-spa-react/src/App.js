3bce5978802a669338cc41320c486bdc2495eb69
Tanisha <Tanisharampersad24@gmail.com>
2024-10-13 13:14:09 +0200

    Added routes to components pages

--- amaranths-spa-react/src/App.js
+++ amaranths-spa-react/src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import hamburger from './images/hamburger.png';
+import { Link, Routes, Route } from 'react-router-dom';
+import ErrorPage from './components/Errorpage';
+//import Home from './components/home';
+// import specials from './components/specials'
+// import Packages from './components/packages';
+// import Massages from './components/massages';
+// import Contact from './components/contact';
 
 function App() {
   return (
@@ -9,21 +16,41 @@ function App() {
         <ul>
         <img className="menu" src={hamburger} alt="hamburger menu"></img>
           <p>|</p>
-          <li>Home</li>
-          <li>Specials</li>
-          <li>Packages</li>
-          <li>Massages</li>
-          <li>Contact</li>
-          <h4>Amaranth</h4>
+          <li>
+            <Link className='link'href="/">Home</Link>
+          </li>
+          <li>
+            <Link className='link' href="/specials">Specials</Link>
+          </li>
+          <li>
+            <Link className='link' href="/packages">Packages</Link>
+          </li>
+
+          <li>
+            <Link className='link' href="/massages">Massages</Link>
+          </li>
+
+          <li>
+            <Link className='link' href="/contact">Contact</Link>
+          </li>
+          <h4>
+            <Link className='link' href="/">Amaranth</Link>
+          </h4>
         </ul>
 
         <button className="booking">Book your stay</button>
-        
-        
       
       </nav>
 
-    
+      <Routes>
+          <Route path='/' errorElement={<ErrorPage />} /> {/*for each route add the errorElement if the url doesnt work */}
+          <Route path='#' /*element={<Home/>}*/ errorElement={<ErrorPage />} />
+          <Route path='#' /*element={<Specials />}*/ errorElement={<ErrorPage />} />
+          <Route path='#' /*element={<Packages />}*/ errorElement={<ErrorPage />} />
+          <Route path='#' /*element={<Massages />}*/ errorElement={<ErrorPage />} />
+          <Route path='#' /*element={<Contact />}*/ errorElement={<ErrorPage />} />
+          <Route path='*' element={<ErrorPage />} /> {/* The '*' path will match any route that hasn’t been explicitly defined, helping you catch 404 errors more gracefully. */}
+      </Routes>
 
 
     </div>
--- amaranths-spa-react/src/index.js
+++ amaranths-spa-react/src/index.js
@@ -2,6 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/Errorpage';
+
+
+const Router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+            path: '/home',
+          //   element: <home />,
+            errorElement: <ErrorPage />,
+          },
+        {
+          path: '/specials',
+        //   element: <specials />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: '/packages',
+        //   element: <packages />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: '/massages',
+        //   element: <massages />,
+          errorElement: <ErrorPage />,
+        },
+        {
+            path: '/contact' ,
+            // element: <contact />,
+            errorElement: <ErrorPage />,
+          },
+       
+      ]
+    }
+  ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <RouterProvider router={Router} />
+);

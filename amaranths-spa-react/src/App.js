00d5622e57bc47d2c0cc73eee75ded35b2b8baad
Tanisha <Tanisharampersad24@gmail.com>
2024-10-14 13:20:28 +0200

    Added the footer and styled it

--- amaranths-spa-react/src/App.js
+++ amaranths-spa-react/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import hamburger from './images/hamburger.png';
 import { Link, Routes, Route } from 'react-router-dom';
 import ErrorPage from './components/Errorpage';
+import star from './images/star.png';
+import copyright from './images/copyright (1).png';
 //import Home from './components/home';
 // import specials from './components/specials'
 // import Packages from './components/packages';
@@ -53,6 +55,85 @@ function App() {
           <Route path='#' /*element={<Contact />}*/ errorElement={<ErrorPage />} />
           <Route path='*' element={<ErrorPage />} /> {/* The '*' path will match any route that hasn’t been explicitly defined, helping you catch 404 errors more gracefully. */}
       </Routes>
+
+
+      <footer>
+        <div className='contact'>
+
+          <div className='menu-footer'>
+            <p>Menu</p>
+            <ul>
+              <li><Link className='links'>Home</Link></li>
+              <li><Link className='links'>Specials</Link></li>
+              <li><Link className='links'>Massage</Link></li>
+              <li><Link className='links'>Contact</Link></li>
+            </ul>
+          </div>
+
+
+          <div className='hours-footer'>
+            <p>Hours</p>
+            <ul>
+              <li>Mon–Fri: 10:00am –21:00pm</li>
+              <br/>
+              <li>Sat - Sun: 08:00am-21:00pm</li>
+
+              <Link className='callandbook'>Call and Book Anytime</Link>
+            </ul>
+          </div>
+
+
+          <div className='contact-footer'>
+            <p className='spa'>Contact</p>
+
+            <p>Amaranth Spa and Massage</p>
+            <br/>
+
+            <p>19, 23 Soi Sukhumvit 19,</p>
+            <p>Khlong Toei Nuea, Watthana,</p>
+            <p>Bangkok 10110</p>
+            <br/>
+
+            <p>+66818230296</p>
+            <br/>
+            <p>Line ID & Kakao ID: josylee</p>
+            <p>Mail: josie835@gmail.com</p>
+          </div>
+
+
+        </div>
+
+
+
+        <div className='second-footer'>
+          
+          <div className='reviews'>
+              <div className='review-stars'>
+                <img src={star} alt="stars"></img>
+                <img src={star} alt="stars"></img>
+                <img src={star} alt="stars"></img>
+                <img src={star} alt="stars"></img>
+                <img src={star} alt="stars"></img>
+              </div>
+
+              <p><Link className='link'><strong>5.0/</strong>555 reviews</Link></p>
+          </div>
+
+
+          <div class="socials">
+            <ul>
+              <li><Link class="social-link">Instagram</Link></li>
+              <li><Link class="social-link">Tripadvisor</Link></li>
+            </ul>
+          </div>
+
+
+          <div class="copyright">
+            <p><img src={copyright} alt="copyright"/>2024 Amaranth</p>
+          </div>
+
+        </div>
+      </footer>
 
 
     </div>
